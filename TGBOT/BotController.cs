afbf30d976d07c6df0e5126dba83ba71e541acdb
darklight1236 <gamasutora@yandex.ru>
2023-11-19 18:19:21 +0400

    first commit

---
+++ TGBOT/BotController.cs
@@ -0,0 +1,35 @@
+﻿using Concentus.Oggfile;
+using Concentus.Structs;
+using GiphyDotNet.Manager;
+using GiphyDotNet.Model.Parameters;
+using GiphyDotNet.Model.Results;
+using NAudio.Wave;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+using Messaage = Telegram.Bot.Types.Message;
+
+
+
+namespace TGBOT
+{
+    internal class BotController
+    {
+    }
+}
